fixed particle filter initial covariance

src/icing_model.py
@@ -38,9 +38,8 @@ class ICINGModel:
         self.SI_augment = SI_augment
 
     def draw_initial_state(self):
-        noise_vars = np.ones((len(self.initial_state),))
-        if self.SI_augment:
-            noise_vars[-1] = (1e-5)**2
+        noise_vars = np.asarray([(0.9*7.5)**2, (0.9*10)**2, (0.9*10)**2, (0.9*1)**2, (0.9*1)**2, (0.9*10)**2, (0.9*1.8e-4)**2])
+        # FIX IF NO STATE AUGMENT
         return rng.multivariate_normal(mean=self.initial_state[:,0], cov=np.diag(noise_vars))
     
     def get_inputs(self, t):

src/particle_filter.py
@@ -224,7 +224,7 @@ class BootstrapParticleFilter:
             fig, ax = model.plot_sim()
             ax[0].plot(self.t_meas, self.G_meas,  marker='o', label='BG measurements', color='k', linestyle='--')
             ax[0].plot(self.t, G_est, label='Fitted BG', color='blue', linestyle='--')
-            ax[0].plot(self.t[1:], G_pred[:-1], label="2 hour ahead prediction", color="green")
+            # ax[0].plot(self.t[1:], G_pred[:-1], label="2 hour ahead prediction", color="green")
             uen_true = model.params["k1"]*np.exp(I_true*model.params["k2"]/model.params["k3"])
             ax[1].plot(self.t, I_est, label='Fitted I', color='blue', linestyle='-')
             ax[1].plot(self.t, uen_true, label="Endogenous Insulin", color="orange", linestyle="-.")